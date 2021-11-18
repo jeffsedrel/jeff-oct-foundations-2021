ac772a40303f8fb4bd43cdce3b298a4c5aa91925
jeffsedrel <jeffsedrel@gmail.com>
2021-11-18 13:31:57 -0600

    more final changes

--- VERS2_Oct_Foundations_Final/js/js.js
+++ VERS2_Oct_Foundations_Final/js/js.js
@@ -1,12 +1,12 @@
 function myFunction(imgs) {
-    // Get the expanded image
-    var expandImg = document.getElementById("expandedImg");
-    // Get the image text
-    var imgText = document.getElementById("imgtext");
-    // Use the same src in the expanded image as the image being clicked on from the grid
-    expandImg.src = imgs.src;
-    // Use the value of the alt attribute of the clickable image as text inside the expanded image
-    imgText.innerHTML = imgs.alt;
-    // Show the container element (hidden with CSS)
-    expandImg.parentElement.style.display = "block";
+    // get the expanded image
+      var expandImg = document.getElementById("expandedImg");
+    // gett the image text
+      var imgText = document.getElementById("imgtext");
+    // use same src in the expanded image as the image being clicked on 
+      expandImg.src = imgs.src;
+    //the value of the alt attribute in the clickable image is the text inside the expanded image
+      imgText.innerHTML = imgs.alt;
+    // show the container element (hidden with css)
+      expandImg.parentElement.style.display = "block";
   }
---
+++ VERS3_Oct_Foundations_Final/js/js.js
@@ -0,0 +1,23 @@
+function myFunction(imgs) {
+    // Get the expanded image
+    var expandImg = document.getElementById("expandedImg");
+    // Get the image text
+    var imgText = document.getElementById("imgtext");
+    // Use the same src in the expanded image as the image being clicked on from the grid
+    expandImg.src = imgs.src;
+    // Use the value of the alt attribute of the clickable image as text inside the expanded image
+    imgText.innerHTML = imgs.alt;
+    // Show the container element (hidden with CSS)
+    expandImg.parentElement.style.display = "block";
+  }
+
+  Example
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
